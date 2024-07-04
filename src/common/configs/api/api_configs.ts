9466532d53bbd439a372d27933d0b135066377d6
yayahc <hamedcuenca5@gmail.com>
2024-07-04 18:50:18 +0000

    fix: merged

--- src/common/configs/api/api_configs.ts
+++ src/common/configs/api/api_configs.ts
@@ -1,4 +1,4 @@
-export const apiUrl='https://70ppn2ql-8280.uks1.devtunnels.ms'
+export const apiUrl='https://4581-154-68-51-79.ngrok-free.app'
 
 export const getUrl=(path:string)=>{
     return `${apiUrl}/v1${path}`
