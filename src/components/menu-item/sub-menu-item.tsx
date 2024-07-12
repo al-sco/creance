a92d362745e18d06b4b754bc9c3945838fe68f4b
yayahc <hamedcuenca5@gmail.com>
2024-07-12 18:22:24 +0000

    feat: make submenu item component scrollable

--- src/components/menu-item/sub-menu-item.tsx
+++ src/components/menu-item/sub-menu-item.tsx
@@ -19,16 +19,20 @@ max-width: 250px;
 padding-right: 1rem;
 `
 
+const SubMenuNameStyled = styled.div`
+  scrollbar-width: none;
+  overflow-y: scroll;
+`
+
 const SubMenuItemComponent = ({ subMenu, isSelected, onPressed }: SubMenuItemProps) => {
     return (
         (
-
             <Link to={subMenu.path.toString()} onClick={() => onPressed(subMenu)}>
                 <BorderedStyle>
                     <StyledMenuItem $textColor={isSelected ? colors.black : colors.lightGray}>
                         <Stack direction='row' spacing={4} justifyContent="start" alignItems="center" >
                             {isSelected && <ChevronRightIcon />}
-                            <span>{subMenu.name}</span>
+                            <SubMenuNameStyled>{subMenu.name}</SubMenuNameStyled>
                         </Stack>
                     </StyledMenuItem>
                 </BorderedStyle>
