f9d3c1db698f92458de3e8f1d8cacdff8c16a8e9
siguialassane <alassanesigui80gmail.com>
2024-07-12 12:02:52 +0000

    chore: define basic mappers for AcDebiteur

--- src/states/signals/creances_providers/AcDebiteur.state.ts
+++ src/states/signals/creances_providers/AcDebiteur.state.ts
@@ -1,20 +1,47 @@
-import { AcCategorieDebiteur, ParameterBaseData } from "../../AcData.types";
+import { AcDebiteur } from "../../AcData.types";
 import ICrudStateProvider from "../parameter_providers/ICrudStateProvider";
 
-class AcDebiteurStateProvider extends ICrudStateProvider<AcCategorieDebiteur> {
-  mapDataToJson(data: ParameterBaseData): {} {
+class AcDebiteurStateProvider extends ICrudStateProvider<AcDebiteur> {
+  mapDataToJson(data: AcDebiteur): {} {
     return {
       categDebCode: data["id"],
       categDebLib:data["libelle"],
+      debAdrpost:data["debAdrpost"],
+      debCel:data["debCel"],
+      debCodeAnc:data["debCodeAnc"],
+      debCodeCharg:data["debCodeCharg"],
+      debDateCtl:data["debDateCtl"],
+      debEmail:data["debEmail"],
+      debFax:data["debFax"],
+      debTelbur:data["debTelbur"],
+      debTeldom:data["debTeldom"],
+      garphysCode:data["garphysCode"],
+      propCode:data["propCode"],
+      typdebCode:data["typdebCode"]
     }
   }
-  mapEntitieFrom(json: any): AcCategorieDebiteur {
+  mapEntitieFrom(json: any): AcDebiteur {
     return {
-      id: json["categDebCode"],
-      code: json["categDebCode"],
-      libelle: json["categDebLib"],
+      id: json["id"],
+      code: json["id"],
+      libelle: json["categDebCode"],
+      debAdrpost:json["debAdrpost"],
+      debCel:json["debCel"],
+      debCodeAnc:json["debCodeAnc"],
+      debCodeCharg:json["debCodeCharg"],
+      debDateCtl:json["debDateCtl"],
+      debEmail:json["debEmail"],
+      debFax:json["debFax"],
+      debTelbur:json["debTelbur"],
+      debTeldom:json["debTeldom"],
+      garphysCode:json["garphysCode"],
+      propCode:json["propCode"],
+      typdebCode:json["typdebCode"]
     };
   }
+
+  
+
 }
 
 const acDebiteurProvider = new AcDebiteurStateProvider(
